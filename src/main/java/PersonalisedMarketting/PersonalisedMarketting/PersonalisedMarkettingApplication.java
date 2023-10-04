16433e2a5aabcb577e0e9ad7d1b7cbdf33c088a5
VIKAS CHAUHAN <55353384+vikaschauhan07@users.noreply.github.com>
2023-10-04 22:29:41 +0530

    Update PersonalisedMarkettingApplication.java

--- src/main/java/PersonalisedMarketting/PersonalisedMarketting/PersonalisedMarkettingApplication.java
+++ src/main/java/PersonalisedMarketting/PersonalisedMarketting/PersonalisedMarkettingApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.models.annotations.OpenAPI30;
 
 @SpringBootApplication
 @OpenAPIDefinition
-@OpenAPI30
 public class PersonalisedMarkettingApplication {
 
 	public static void main(String[] args) {
